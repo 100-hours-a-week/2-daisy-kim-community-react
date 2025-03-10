aa4a91515153c95104fd128c8e711e030d8441bd
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-03-10 09:05:48 +0900

    Chore : Tailwind 라이브러리 설치 및 불필요한 파일 정리

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
