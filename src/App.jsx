bb8e9175ba0e46a4faef93151bd9807a6f7bd6e2
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-03-15 13:52:09 +0900

    Feat : 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Header from "./components/common/Header";
+import SignUp from "./pages/signup/Signup";
+import PostList from "./pages/post/PostList";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/postList" element={<PostList />} />
       </Routes>
     </>
   );
