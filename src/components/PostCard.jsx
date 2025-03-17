a389b8e1fc31be4198fc2813235a8e00175a1154
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-03-17 14:33:36 +0900

    Feat : 게시글 리스트 페이지 퍼블리싱

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+
+export default function PostCard({
+  title,
+  likes,
+  comments,
+  views,
+  date,
+  profileImage,
+  author,
+}) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+
+      <InfoSection>
+        <InfoText>
+          좋아요 {likes} 댓글 {comments} 조회수 {views}
+        </InfoText>
+        <CreatedDate>{date}</CreatedDate>
+      </InfoSection>
+
+      <AuthorSection>
+        <ProfileImage src={profileImage} />
+        <AuthorName>{author}</AuthorName>
+      </AuthorSection>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  padding: 10px;
+  box-sizing: border-box;
+`;
+
+const Title = styled.p`
+  padding: 10px;
+  font-size: 16px;
+  font-weight: bold;
+  color: black;
+  margin: 0;
+`;
+
+const InfoSection = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 10px;
+  padding-bottom: 10px;
+  box-sizing: border-box;
+`;
+
+const InfoText = styled.p`
+  font-size: 12px;
+`;
+
+const CreatedDate = styled.p`
+  font-size: 12px;
+`;
+
+const AuthorSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 10px;
+
+  border-top: 1px solid lightgray;
+`;
+
+const ProfileImage = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: lightgray;
+  background-image: ${(props) => (props.src ? `url(${props.src})` : "none")};
+  background-size: cover;
+  background-position: center;
+`;
+
+const AuthorName = styled.p`
+  font-size: 12px;
+  font-weight: 700;
+`;
--- src/pages/post/PostList.jsx
+++ src/pages/post/PostList.jsx
@@ -1,3 +1,81 @@
+import styled from "styled-components";
+import PostCard from "../../components/PostCard";
+
 export default function PostList() {
-  return <></>;
+  const postDummy = [
+    {
+      id: 1,
+      title: "제목 1",
+      likes: 10,
+      comments: 20,
+      views: 30,
+      date: "2025-03-10 12:03:48",
+      profileImage: "https://placehold.co/",
+      author: "작성자1",
+    },
+  ];
+
+  return (
+    <Container>
+      <Wrapper>
+        <TitleText>
+          안녕하세요, <br />
+          아무 말 대잔치 <b>게시판</b>입니다.
+        </TitleText>
+        <WriteButton>게시글 작성</WriteButton>
+        {postDummy.map((post) => (
+          <PostCard
+            key={post.id}
+            title={post.title}
+            likes={post.likes}
+            comments={post.comments}
+            views={post.views}
+            date={post.date}
+            profileImage={post.profileImage}
+            author={post.author}
+          />
+        ))}
+      </Wrapper>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 100vw;
+  height: calc(100vh - 60px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  height: calc(100vh - 60px);
+  background-color: pink;
+`;
+
+const TitleText = styled.p`
+  font-size: 18px;
+  font-weight: 400;
+  text-align: center;
+  color: black;
+
+  .b {
+    font-weight: 700;
+  }
+`;
+
+const WriteButton = styled.button`
+  width: 100px;
+  padding: 10px;
+  font-size: 12px;
+  font-weight: 800;
+  border-radius: 40px;
+  align-self: flex-end;
+  margin-bottom: 20px;
+  color: white;
+  background-color: #aea0eb;
+`;
