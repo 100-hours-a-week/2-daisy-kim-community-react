fdd69e01b3f8f471e22a7a875b866aa8a7e94997
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-03-18 20:48:58 +0900

    Feat : 비밀번호 수정 페이지 퍼블리싱

--- src/pages/account/EditPassword.jsx
+++ src/pages/account/EditPassword.jsx
@@ -1,3 +1,106 @@
+import { useAuthForm } from "@hooks/useAuthForm";
+import styled from "styled-components";
+
 export default function EditPassword() {
-  return <></>;
+  const {
+    password,
+    confirmPassword,
+    passwordError,
+    confirmPasswordError,
+    handlePasswordChange,
+    handleConfirmPasswordChange,
+  } = useAuthForm();
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title>비밀번호 수정</Title>
+
+        <FormText>비밀번호</FormText>
+        <FormInput
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="비밀번호를 입력하세요"
+        />
+        {passwordError && <ErrorText>{passwordError}</ErrorText>}
+
+        <FormText>비밀번호 확인</FormText>
+        <FormInput
+          type="password"
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+          placeholder="비밀번호를 한번 더 입력하세요"
+        />
+        {confirmPasswordError && <ErrorText>{confirmPasswordError}</ErrorText>}
+
+        <EditButton>수정하기</EditButton>
+      </Wrapper>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: calc(100vh - 60px);
+  justify-content: center;
+  padding-top: 100px;
+  overflow-y: hidden;
+  box-sizing: border-box;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  align-items: center;
+`;
+
+const Title = styled.p`
+  font-size: 16px;
+  font-weight: 700;
+  margin-bottom: 20px;
+`;
+
+const FormText = styled.p`
+  width: 300px;
+
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+const FormInput = styled.input`
+  width: 300px;
+  border: 1px solid black;
+  border-radius: 8px;
+  color: black;
+  background-color: transparent;
+  padding: 8px;
+  box-sizing: border-box;
+
+  &::placeholder {
+    font-size: 12px;
+    font-weight: 500;
+  }
+`;
+
+const ErrorText = styled.p`
+  width: 300px;
+  justify-content: start;
+  color: red;
+  font-size: 12px;
+  font-weight: 600;
+`;
+
+const EditButton = styled.button`
+  width: 300px;
+  padding: 8px;
+  border: 1px solid white;
+  border-radius: 8px;
+  background-color: #a996e7;
+  color: white;
+  font-size: 12px;
+  font-weight: 700;
+  margin-top: 40px;
+`;
--- src/pages/account/EditProfile.jsx
+++ src/pages/account/EditProfile.jsx
@@ -48,6 +48,9 @@ const Container = styled.div`
   width: 100vw;
   height: calc(100vh - 60px);
   justify-content: center;
+  padding-top: 100px;
+  overflow-y: hidden;
+  box-sizing: border-box;
 `;
 
 const Wrapper = styled.div`
